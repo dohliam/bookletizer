495339bb65d4e25f8d66351d5f1729a2d050c694
dohliam <dohliam@gmail.com>
2019-11-25 22:15:03 -0800

    add option to specify filename

--- bookletizer.rb
+++ bookletizer.rb
@@ -10,11 +10,17 @@ options = {}
 OptionParser.new do |opts|
   opts.banner = "Usage: bookletizer.rb [options]"
 
+  opts.on("-f", "--filename FILE", "Source PDF filename") { |v| options[:filename] = v }
   opts.on("-l", "--letter-size", "Specify paper size as US letter (default A4)") { options[:letter_size] = true }
 
 end.parse!
 
-pdf = ARGV[0]
+pdf = options[:filename]
+if !pdf
+  abort("  Please specify an input PDF file.")
+elsif !File.exist?(pdf)
+  abort("  Specified file not found.")
+end
 
 book_name = get_book_name(pdf) + "-booklet.pdf"
 
@@ -29,7 +35,9 @@ end
 
 `pdfbook#{letter_size} #{blanks}`
 
-File.rename("tmp_blanks-book.pdf", "output/" + book_name)
+out_filename = "output/" + book_name
+
+File.rename("tmp_blanks-book.pdf", out_filename)
 File.delete(blanks)
 
 puts "  Created booklet #{book_name}"
